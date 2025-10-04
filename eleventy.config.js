610943c1b463a516ffd2d00f22ecfbb354437d28
Ming Liang <khongmingliang@icloud.com>
2025-10-04 23:19:36 +0800

    first commit

---
+++ eleventy.config.js
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+import pluginWebc from "@11ty/eleventy-plugin-webc";
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import tailwindcss from "@tailwindcss/postcss";
+import postcss from "postcss";
+
+export default async function (eleventyConfig) {
+  // plugins
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "src/_includes/components/**/*.webc",
+  });
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  // tailwind + daisyui
+  eleventyConfig.on("eleventy.before", async () => {
+    const tailwindInputPath = path.resolve("./src/styles/index.css");
+    const tailwindOutputPath = "./_site/styles/index.css";
+    const cssContent = fs.readFileSync(tailwindInputPath, "utf8");
+    const outputDir = path.dirname(tailwindOutputPath);
+
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const result = await postcss([tailwindcss()]).process(cssContent, {
+      from: tailwindInputPath,
+      to: tailwindOutputPath,
+    });
+
+    fs.writeFileSync(tailwindOutputPath, result.css);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+    templateFormats: ["webc", "md"],
+  };
+}
---
+++ src/posts/04-10-25-hello-world.md
@@ -0,0 +1,16 @@
+---
+title: "Hello, World!"
+created: "2025-10-04"
+updated: "2025-10-04"
+---
+
+Welcome to my site! I'm **Ming Liang**, a software engineer based in Singapore.  
+I started this site as a way to document my journey in software engineering and to showcase some of the work I’ve done along the way.
+
+This site is built using [Eleventy (11ty)](https://www.11ty.dev/), [WebC](https://webc-lang.org/), and [Tailwind CSS](https://tailwindcss.com/), and is deployed on [GitHub Pages](https://pages.github.com/). It’s my first time combining these tools, and I’m excited to explore what I can build with them.
+
+Over the next few posts, I’ll be sharing how I put this site together and some of the things I learned throughout the process.
+
+While it’s still a work in progress, I hope you’ll find the content here both interesting and useful.
+
+Feel free to follow along as I document my experiments and learning journey.
